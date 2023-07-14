8649cb29e5ea40bdd405ad5f319a316047258ac4
sharonvijay <94788632+sharonvijay@users.noreply.github.com>
2023-07-14 11:43:21 +0530

    chatControllers.js

--- server/controllers/chatControllers.js
+++ server/controllers/chatControllers.js
@@ -49,8 +49,27 @@ const accessChat = asyncHandler(async (req, res) => {
 		}
 	}
 });
+const fetchChats = asyncHandler(async (req, res) => {
+	try {
+		Chat.find({ users: { $elemMatch: { $eq: req.user._id } } })
+			.populate("users", "-password")
+			.populate("groupAdmin", "-password")
+			.populate("latestMessage")
+			.sort({ updatedAt: -1 })
+			.then(async (results) => {
+				results = await User.populate(results, {
+					path: "latestMessage.sender",
+					select: "name pic email",
+				});
+				res.status(200).send(results);
+			});
+	} catch (error) {
+		res.status(400);
+		throw new Error(error.message);
+	}
+});
+
 const createGroupChat = asyncHandler(async (req, res) => {});
-const fetchChats = asyncHandler(async (req, res) => {});
 const renameGroup = asyncHandler(async (req, res) => {});
 const removeFromGroup = asyncHandler(async (req, res) => {});
 const addToGroup = asyncHandler(async (req, res) => {});
