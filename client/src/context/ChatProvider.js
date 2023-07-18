103fbeace0352a1228e20d501176745ebbbab71e
sharonvijay <94788632+sharonvijay@users.noreply.github.com>
2023-07-18 21:08:14 +0530

    ChatProvider

--- client/src/context/ChatProvider.js
+++ client/src/context/ChatProvider.js
@@ -4,8 +4,10 @@ import { useNavigate } from "react-router-dom";
 const ChatContext = createContext();
 
 const ChatProvider = ({ children }) => {
+	const [selectedChat, setSelectedChat] = useState();
 	const [user, setUser] = useState();
-
+	const [notification, setNotification] = useState([]);
+	const [chats, setChats] = useState();
 	const navigate = useNavigate();
 
 	useEffect(() => {
@@ -13,11 +15,22 @@ const ChatProvider = ({ children }) => {
 		setUser(userInfo);
 
 		if (!userInfo) {
-			navigate.push("/");
+			navigate("/");
 		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [navigate]);
 	return (
-		<ChatContext.Provider value={{ user, setUser }}>
+		<ChatContext.Provider
+			value={{
+				selectedChat,
+				setSelectedChat,
+				user,
+				setUser,
+				notification,
+				setNotification,
+				chats,
+				setChats,
+			}}>
 			{children}
 		</ChatContext.Provider>
 	);
