fa5705fdf5a40706de02d5fe66e21026b9006266
sharonvijay <94788632+sharonvijay@users.noreply.github.com>
2023-07-19 19:45:30 +0530

    App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,17 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Chatpage from "./pages/Chatpage";
+import SignIn from "../src/components/authentication/SignIn";
+import SignUp from "../src/components/authentication/SignUp";
 import "./App.css";
 function App() {
 	return (
 		<div className="App">
 			<Routes>
-				<Route path="/" element={<Homepage />} exact />
-				<Route path="/chats" component={Chatpage} />
+				<Route path="/" element={<Homepage />} />
+				<Route path="/chats" element={<Chatpage />} />
+				<Route path="/signin" element={<SignIn />} />
+				<Route path="/signup" element={<SignUp />} />
 			</Routes>
 		</div>
 	);
