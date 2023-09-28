6062574926478a5af23cc7912da3fb36f67cfebd
sharonvijay <94788632+sharonvijay@users.noreply.github.com>
2023-09-28 14:03:57 +0530

    correction

--- client/src/context/ChatProvider.js
+++ client/src/context/ChatProvider.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useState } from "react";
 // import { useNavigate } from "react-router-dom";
 
 const ChatContext = createContext();
